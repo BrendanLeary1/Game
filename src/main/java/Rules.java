a7ce7c15cf5d5704a3366d98b129f122fb89ad04
BrendanLeary1 <brlea@udel.edu>
2023-05-03 10:55:57 -0400

    h

--- src/main/java/Rules.java
+++ src/main/java/Rules.java
@@ -1,17 +1,30 @@
+/**
+ * <h1>Lab7</h1>
+ * <h2>CISC 181-020L Spring 2023</h2>
+ * <h3>University of Delaware</h3>
+ * Rules is a class that sets up the basic turn mechanics for the main game.
+ * We can check if the Unit trying to perform an action is performing a valid
+ * action.
+ *
+ * @author Brendan Leary, Jacob Whitman, Meaghan Roth
+ *
+ * @since 2023-05-03
+ */
+
 public class Rules {
     public static boolean checkValidAction(Game game, int fromRow, int fromCol, int toRow, int toCol, char action)  {
          // Checks if the action char is not one of the valid options (M,S,R,A)
-         if(action != 'M' && action != 'S' && action != 'R' && action != 'A') {
+         if (!( (action == 'M' || action == 'S') || (action == 'R' || action == 'A')) ) {
              return false;
          } else {
              // Assesses whether the From Unit is in bounds and contains a unit
-             private GameBoard board = game.getGameBoard();
-             private BoardSquare[][] squares = board.getSquares();
-             private Team currentTeam = game.getCurrentPlayer().getPlayersTeam();
-             private Team opposingTeam = game.getOpponentPlayer().getPlayersTeam();
+             GameBoard board = game.getGameBoard();
+             BoardSquare[][] squares = board.getSquares();
+             Team currentTeam = game.getCurrentPlayer().getPlayersTeam();
+             Team opposingTeam = game.getOpponentPlayer().getPlayersTeam();
              if(board.inBounds(fromRow, fromCol) && !(squares[fromRow][fromCol].isEmptySpace())) {
                  // Checks whether the From Unit is from the current player's team
-                 private Unit fromUnit = squares[fromRow][fromCol].getUnit();
+                 Unit fromUnit = squares[fromRow][fromCol].getUnit();
                  if(currentTeam.getTeamColor().equals(fromUnit.getTeamColor())) {
                      // ***** Move
                      if (action == 'M') {
@@ -42,15 +55,16 @@ public class Rules {
                      } else {
                          // Checks whether the To Unit is not empty
                          if(!squares[toRow][toCol].isEmptySpace()) {
-                            private Unit toUnit =  squares[toRow][toCol].getUnit();
+                            Unit toUnit =  squares[toRow][toCol].getUnit();
                             // Checks whether the To Unit is of the opposing team
                             if(opposingTeam.getTeamColor().equals(toUnit.getTeamColor())) {
                                 // ***** Recruit
                                 if(action == 'R') {
                                     // Checks whether the from unit can recruit
-                                    if(fromUnit instanceof BartSimpsonUnit) {
+                                    if(fromUnit instanceof Recruiter) {
                                         // Checks whether the from unit has a valid recruit path
-                                        if(fromUnit.validRecruitPath(fromRow, fromCol, toRow, toCol)) {
+                                        // must cast from unit to Recruiter
+                                        if(((Recruiter) fromUnit).validRecruitPath(fromRow, fromCol, toRow, toCol)) {
                                             return true;
                                         } else {
                                             return false;
@@ -61,9 +75,10 @@ public class Rules {
                                 // ***** Attack
                                 } else {
                                     // Checks whether the from unit can attack
-                                    if(fromUnit instanceof TomJerryUnit) {
+                                    if(fromUnit instanceof Attacker) {
                                         // Checks whether the from unit has a valid attack path
-                                        if(fromUnit.validAttackPath(fromRow, fromCol, toRow, toCol)) {
+                                        // must cast from unit to Attacker
+                                        if(((Attacker) fromUnit).validAttackPath(fromRow, fromCol, toRow, toCol)) {
                                             return true;
                                         } else {
                                             return false;
