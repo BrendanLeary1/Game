978f36d63c26dd12e11e3c8e1c84c24a851d178a
meaghanr-CS <meaghanr@udel.edu>
2023-05-03 09:56:00 -0400

    added things to Controller

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,3 +1,54 @@
 public class Controller {
-    Game game;
+    private Game game;
+    private TextView view;
+
+    public Game setUpGameModel(){
+        // Create 4 pieces for team A
+        // Load the pieces in an ArrayList
+        ArrayList<Unit> piecesTeamA = new ArrayList<Unit>();
+
+
+        BartSimpsonUnit bs = new BartSimpsonUnit();
+        bs.setTeamColor("Blu");
+
+
+        TomJerryUnit tj = new TomJerryUnit();
+        tj.setTeamColor("Blu");
+
+
+        piecesTeamA.add(bs);
+        piecesTeamA.add(tj);
+
+
+        // Create a team object
+        Team teamA = new Team("Blu",piecesTeamA);
+        Player playerOne = new Player(1, true, teamA);
+
+
+        // Create 4 pieces for team B
+        // Load the pieces in an ArrayList
+        ArrayList<Unit> piecesTeamB = new ArrayList<Unit>();
+
+
+        BartSimpsonUnit bs2 = new BartSimpsonUnit();
+        bs2.setTeamColor("Red");
+
+
+        TomJerryUnit tj2 = new TomJerryUnit();
+        tj2.setTeamColor("Red");
+
+
+        piecesTeamB.add(bs2);
+        piecesTeamB.add(tj2);
+
+
+        // Create a team object
+        Team teamB = new Team("Red",piecesTeamB);
+        Player playerTwo = new Player(2, false, teamB);
+
+
+        // Create an instance of the game
+        return new Game(8, 8, playerOne, playerTwo);
+    }
+
 }
