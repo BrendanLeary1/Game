cc785ceee6163ed225ee7601ac04b7bb75a386ae
meaghanr-CS <meaghanr@udel.edu>
2023-05-19 12:34:28 -0400

    fixed ActionBark

--- src/main/java/ActionBark.java
+++ src/main/java/ActionBark.java
@@ -6,6 +6,8 @@
  * ActionBark is a class that extends Action. It allows for a Unit that extends it
  * to "bark" at other Units, which can scare them into backing up one space from the barking Unit
  * only if they are in a cardinal direction (not diagonal) from the barking Unit and in the Unit's bark range.
+ * If the opposing unit is on the edge of the board and barked at, they are removed from the
+ * board.
  *
  * Visual of Barker 'S' with a bark range of 1 (range represented by 'b')
  *   b
@@ -51,14 +53,18 @@ public class ActionBark extends Action {
                 char direction = ((ScoobyDooUnit) barker).barkDirection(fromRow, fromCol, toRow, toCol);
                 Unit temp = game.getGameBoard().getSquares()[toRow][toCol].getUnit();
                 game.getGameBoard().getSquares()[toRow][toCol].removeUnit();
-                if (direction == 'L') {
-                    game.getGameBoard().getSquares()[toRow][toCol-1].setUnit(temp);
-                } else if (direction == 'R') {
-                    game.getGameBoard().getSquares()[toRow][toCol+1].setUnit(temp);
-                } else if (direction == 'U') {
-                    game.getGameBoard().getSquares()[toRow-1][toCol].setUnit(temp);
-                } else if (direction == 'D') {
-                    game.getGameBoard().getSquares()[toRow+1][toCol].setUnit(temp);
+                int colLength = game.getBoardSquares().length;
+                int rowLength = game.getBoardSquares()[0].length;
+                if(toRow != 0 && toCol != 0 && toRow != rowLength-1 && toCol != colLength-1) {
+                    if (direction == 'L') {
+                        game.getGameBoard().getSquares()[toRow][toCol - 1].setUnit(temp);
+                    } else if (direction == 'R') {
+                        game.getGameBoard().getSquares()[toRow][toCol + 1].setUnit(temp);
+                    } else if (direction == 'U') {
+                        game.getGameBoard().getSquares()[toRow - 1][toCol].setUnit(temp);
+                    } else if (direction == 'D') {
+                        game.getGameBoard().getSquares()[toRow + 1][toCol].setUnit(temp);
+                    }
                 }
                 System.out.println(game.toString());
                 game.changeTurn();
